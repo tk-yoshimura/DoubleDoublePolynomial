fd13ce3e09887942e9ea0590a1891ced62ff265b
tk-yoshimura <yoshimura8192@gmail.com>
2026-01-17 00:52:16 +0900

    add: AssemblyInfo

---
+++ DoubleDoublePolynomial/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DoubleDouble")]
+[assembly: AssemblyDescription("Double-Double Polynomial Implements")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DoubleDoublePolynomial")]
+[assembly: AssemblyCopyright("Copyright © T.Yoshimura 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("24B23817-B39D-4AFE-8F33-D107C7F09501")]
+
+[assembly: AssemblyVersion("1.0.0.*")]
